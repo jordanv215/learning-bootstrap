5705bd116639c7afdb9e37ce660f7806b2167d61
Jordan Vinson <jvinson3@cnm.edu>
2016-05-06 10:07:48 -0600

    Coded all links and info. Ready to begin coding html for web page

--- index.php
+++ index.php
@@ -36,5 +36,119 @@
 
 		<!-- time to actually add javascript -->
 
+		<title>Hello Bootstrap</title>
 	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+
+			<header>
+				<div class="container">
+					<!-- begin bootstrap navbar -->
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<i class="fa fa-th fa-2x fa-spin" aria-hidden="true"></i>
+						</button>
+						<a class="navbar-brand" href="#">Bootstrap Sample</a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="main-menu">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="/">Home</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">More Items <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="http://google.com">Google.com</a></li>
+									<li><a href="https://senator-arlo.bowtied.io">Feel The Fuzzy</a></li>
+									<li><a href="https://reddit.com/r/softwaregore">REddit</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div>
+			</header>
+
+			<main>
+				<div class="container">
+					<div class="row">
+
+						<!--welcome section-->
+						<div class="col-xs-12 well text-center">
+							<h1>Welcome to my website! Yay!</h1>
+							<p>Some text down here...</p>
+						</div>
+
+						<!--1st content section-->
+						<div class="row">
+							<div class="col-md-3">
+								<div class="well">
+									<h4>Section 1</h4>
+									<p>My left donut is missing, as is my right use lap as chair. Lick the other cats scamper sit in box meowzer!. Immediately regret falling into bathtub unwrap toilet paper hola te quiero. Eat from dog's food white cat sleeps on a black shirt hide head under blanket so no one can see.</p>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="well">
+									<h4>Section 2</h4>
+									<p>My left donut is missing, as is my right use lap as chair. Lick the other cats scamper sit in box meowzer!. Immediately regret falling into bathtub unwrap toilet paper hola te quiero. Eat from dog's food white cat sleeps on a black shirt hide head under blanket so no one can see.</p>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="well">
+									<h4>Section 3</h4>
+									<p>My left donut is missing, as is my right use lap as chair. Lick the other cats scamper sit in box meowzer!. Immediately regret falling into bathtub unwrap toilet paper hola te quiero. Eat from dog's food white cat sleeps on a black shirt hide head under blanket so no one can see.</p>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="well">
+									<h4>Section 4</h4>
+									<p>My left donut is missing, as is my right use lap as chair. Lick the other cats scamper sit in box meowzer!. Immediately regret falling into bathtub unwrap toilet paper hola te quiero. Eat from dog's food white cat sleeps on a black shirt hide head under blanket so no one can see.</p>
+								</div>
+							</div>
+						</div>
+
+						<!--2nd content section-->
+						<div class="row">
+							<div class="col-md-6">
+								<div class="well">
+									<h4>section 5</h4>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="well">
+									<h4>section 6</h4>
+								</div>
+							</div>
+						</div>
+					</div><!--/.row-->
+				</div><!--/.container-->
+			</main>
+
+		</div>
+
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="foot-content">
+							content here
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="foot-content">
+							content here
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="copyright text-center">
+							&copy; 2016 Foo!
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
 </html>
