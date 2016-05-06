fb35c067f3cfda3130a747d9336f3a9aa1a328c2
Jordan Vinson <jvinson3@cnm.edu>
2016-05-06 15:19:21 -0600

    CSS not working. Link is correct. No idea what problem is as I just tested by copying everything 100% exactly from Rochelle's code and it is still broken.

--- index.php
+++ index.php
@@ -3,38 +3,38 @@
 	<head>
 		<meta charset="UTF-8">
 
-		<!-- sets IE rending to IE Edge in case someone actually uses IE -->
+		<!-- sets IE rendering to IE-EDGE -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
 		<!-- sets viewport width to device width, scaling 1:1 -->
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-		<!-- latest compiled and minified CSS -->
+		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-		<!-- optional theme -->
+		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
-		<!-- FontAwesome! -->
+		<!-- fontawesome -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
 
-		<!-- HTML shiv and Respond.js for the IE8 support of HTML5 elements and media queries -->
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[If IE 9]>
+		<!--[if lt IE 9]>
 		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
 
-		<!-- Custom CSS -->
+		<!-- OUR CUSTOM CSS -->
 		<link rel="stylesheet" href="css/style.css" type="text/css">
 
-		<!-- Jquery -->
+		<!-- jQuery -->
 		<script src="https://code.jquery.com/jquery-2.2.3.min.js"   integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo="   crossorigin="anonymous"></script>
 
-		<!-- latest JavaScript -->
+		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
-		<!-- time to actually add javascript -->
+		<!-- ADD YOUR JAVASCRIPT/JQUERY DOWN HERE -->
 
 		<title>Hello Bootstrap</title>
 	</head>
