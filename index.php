38afbffdf43856a77d0edb32c4aacdbdefad23a7
Jordan Vinson <jvinson3@cnm.edu>
2016-05-07 12:33:25 -0600

    changed dimensions again

--- index.php
+++ index.php
@@ -85,7 +85,7 @@
 							<div class="col-md-3">
 								<div class="well">
 									<h4>Section 1</h4>
-									<img class="flt-right img-tiny" src="images/bmw.jpg" alt="bmwbody" height="250" widgth="250">
+									<img class="flt-right img-tiny" src="images/bmw.jpg" alt="bmwbody" height="175" widgth="175">
 								</div>
 							</div>
 							<div class="col-md-3">
